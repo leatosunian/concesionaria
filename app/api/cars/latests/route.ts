fd3d799d6b8bb5279076829c8d5021bf319f681e
Lea <leandrotosunian@hotmail.com>
2024-10-01 14:14:24 -0300

    .

--- app/api/cars/latests/route.ts
+++ app/api/cars/latests/route.ts
@@ -2,16 +2,19 @@ import CarModel, { ICar } from "@/app/models/car";
 import connectDB from "@/lib/db";
 import { NextResponse } from "next/server";
 
-
-
 // GET ALL CARS
 export async function GET(context: any) {
   const { params } = context;
   await connectDB();
   try {
-    const lastCars = await CarModel.find()
-      .sort({ createdAt: -1 }) 
-      .limit(10); 
+    const lastCars = await CarModel.find().sort({ createdAt: -1 }).limit(10);
+    console.log(lastCars);
+    
+    // Verifica si se encontraron documentos
+    if (lastCars.length === 0) {
+      return NextResponse.json({ msg: "NO_CARS_FOUND" }, { status: 404 });
+    }
+
     return NextResponse.json(lastCars);
   } catch (error) {
     return NextResponse.json({ msg: "ERROR_GET_CAR" });
