a2e1709a2abbf16e23aa21e89223b30a88aaef6b
Lea <leandrotosunian@hotmail.com>
2024-12-01 20:41:00 -0300

    open graph test 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,22 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import SessionWrapper from "@/components/SessionWrapper";
-
+import image from './opengraph-image.png'
 const inter = Inter({ subsets: ["latin"],  display: "swap",
 });
 
 export const metadata: Metadata = {
   title: "Concesionaria | Inicio",
   description: "Encontrá el auto de tus sueños al mejor precio.",
+  openGraph: {
+    images: [
+      {
+        url: image.src,
+        width: image.width,
+        height: image.height
+      },
+    ],
+  }
 };
 
 export default function RootLayout({
