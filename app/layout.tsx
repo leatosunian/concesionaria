bf4d37e8d3d9a52766df030e47e45a81d33baca7
Lea <leandrotosunian@hotmail.com>
2024-12-01 21:09:46 -0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,22 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import SessionWrapper from "@/components/SessionWrapper";
-import image from './opengraph-image.png'
+
 const inter = Inter({ subsets: ["latin"],  display: "swap",
 });
 
 export const metadata: Metadata = {
   title: "Concesionaria | Inicio",
-  description: "Encontrá el auto de tus sueños al mejor precio.",
-  openGraph: {
-    images: [
-      {
-        url: image.src,
-        width: image.width,
-        height: image.height
-      },
-    ],
-  }
+  description: "Encuentra el auto de tus sueños al mejor precio.",
 };
 
 export default function RootLayout({
